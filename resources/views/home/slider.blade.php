0a6316d0967e9d1e40f264931b7e6818350fd150
bilalabdulhadi <bilalabdulhadipro@gmail.com>
2022-04-15 15:41:56 +0300

    Template Implementation

---
+++ resources/views/home/slider.blade.php
@@ -0,0 +1,36 @@
+<div class="hero">
+    <div class="container-wrap d-flex justify-content-end align-items-end">
+        <a href="https://www.youtube.com/watch?v=ism1XqnZJEg" class="icon-video popup-vimeo d-flex justify-content-center align-items-center">
+            <span class="ion-ios-play play"></span>
+        </a>
+    </div>
+    <section class="home-slider owl-carousel">
+        <div class="slider-item" style="background-image:url({{asset('assets')}}/images/bg_1.jpg);">
+            <div class="overlay"></div>
+            <div class="container">
+                <div class="row no-gutters slider-text align-items-center">
+                    <div class="col-md-8 ftco-animate">
+                        <div class="text mb-5 pb-5">
+                            <h1 class="mb-3">Roxandrea</h1>
+                            <h2>More than a hotel... an experience</h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="slider-item" style="background-image:url({{asset('assets')}}/images/bg_2.jpg);">
+            <div class="overlay"></div>
+            <div class="container">
+                <div class="row no-gutters slider-text align-items-center">
+                    <div class="col-md-8 ftco-animate">
+                        <div class="text mb-5 pb-5">
+                            <h1 class="mb-3">Experience Epic Beauty</h1>
+                            <h2>Roxandrea Hotel &amp; Resort</h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
--- resources/views/layouts/frontbase.blade.php
+++ resources/views/layouts/frontbase.blade.php
@@ -1,20 +1,51 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-    <title>App Name - @yield('title')</title>
-    @yield('head')
+    <title>@yield("title")</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,500,600,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Playfair+Display:400,400i,700,700i" rel="stylesheet">
+
+    <link rel="stylesheet" href="{{asset('assets')}}/css/open-iconic-bootstrap.min.css">
+    <link rel="stylesheet" href="{{asset('assets')}}/css/animate.css">
+
+    <link rel="stylesheet" href="{{asset('assets')}}/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="{{asset('assets')}}/css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="{{asset('assets')}}/css/magnific-popup.css">
+
+    <link rel="stylesheet" href="{{asset('assets')}}/css/aos.css">
+
+    <link rel="stylesheet" href="{{asset('assets')}}/css/ionicons.min.css">
+
+    <link rel="stylesheet" href="{{asset('assets')}}/css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="{{asset('assets')}}/css/jquery.timepicker.css">
+
+
+    <link rel="stylesheet" href="{{asset('assets')}}/css/flaticon.css">
+    <link rel="stylesheet" href="{{asset('assets')}}/css/icomoon.css">
+    <link rel="stylesheet" href="{{asset('assets')}}/css/style.css">
+
+    @yield("head")
 </head>
+
+
 <body>
-<h1>Header</h1>
+@include("home.header")
 
 @section('sidebar')
-    This is the master sidebar.
+    @include("home.sidebar")
 @show
 
-<div class="container">
-    @yield('content')
-</div>
+@section('slider')
 
-<h1>Footer</h1>
+@show
+
+@yield('content')
+
+
+<@include("home.footer")
 @yield('foot')
 </body>
 </html>
